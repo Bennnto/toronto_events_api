b5ac5c73b6256ee7ca2125ad881baa5878de767e
bennnto <ben.promkaew@icloud.com>
2026-05-08 14:36:29 -0400

    Change html templates header

--- event_app/tasks.py
+++ event_app/tasks.py
@@ -5,3 +5,23 @@ from .services import sync_db
 @shared_task
 def refresh_db():
     return sync_db()
+
+
+@shared_task
+def refresh_db_from_jsonld():
+    import requests
+    import os
+    
+    url = "https://raw.githubusercontent.com/TorontoOpenData/events/master/all.jsonld"
+    file_path = "toronto_docs/all.jsonld"
+    
+    resp = requests.get(url)
+    resp.raise_for_status()
+    if resp.status_code == 200:
+        with open(file_path, 'w') as f:
+            f.write(resp.text)
+            
+    from .management.commands.import_jsonld import Command
+    cmd = Command()
+    cmd.handle(file=file_path, commit=True)
+    
